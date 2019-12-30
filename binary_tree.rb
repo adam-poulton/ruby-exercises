e7dacc906e5a7d87c8ffb90c6c1a12a0a10c8cbc
adam-poulton <adam.poulton5@gmail.com>
2019-12-30 18:46:04 +1100

    Complete binary search tree implementation

--- binary_tree.rb
+++ binary_tree.rb
@@ -27,7 +27,7 @@ end
 class Tree
   attr_accessor :root
   def initialize(array)
-    @root = Tree.build_tree(array.uniq.sort)
+    @root = Tree.build_tree(array)
   end
 
   def insert(value, node = @root)
@@ -81,9 +81,73 @@ class Tree
     end
   end
 
+  def level_order(node = @root, results = [], &block)
+    queue = [node]
+    until queue.empty?
+      current = queue.shift
+      block_given? ? yield(current) : results << current.value
+      queue << current.left unless current.left.nil?
+      queue << current.right unless current.right.nil?
+    end
+    results unless block_given?
+  end
+
+  def level_order_rec(node = @root, queue = [], result = [], &block)
+    return if node.nil?
+    block_given? ? yield(node) : result << node.value
+    queue << node.left unless node.left.nil?
+    queue << node.right unless node.right.nil?
+    level_order_rec(queue.shift, queue, result, &block)
+    result unless block_given?
+  end
+
+  def inorder(node = @root, result = [], &block)
+    return if node.nil?
+    inorder(node.left, result, &block)
+    block_given? ? yield(node) : result << node.value
+    inorder(node.right, result, &block)
+    result unless block_given?
+  end
+
+  def preorder(node = @root, result = [], &block)
+    return if node.nil?
+    block_given? ? yield(node) : result << node.value
+    preorder(node.left, result, &block)
+    preorder(node.right, result, &block)
+    result unless block_given?
+  end
+
+  def postorder(node = @root, result = [], &block)
+    return if node.nil?
+    postorder(node.left, result, &block)
+    postorder(node.right, result, &block)
+    block_given? ? yield(node) : result << node.value
+    result unless block_given?
+  end
+
+  def depth(node = @root)
+    return 0 if node.nil?
+    left_depth = depth(node.left)
+    right_depth = depth(node.right)
+    return left_depth + 1 unless right_depth > left_depth
+    return right_depth + 1
+  end
+
+  def balanced?(node = @root)
+    return true if node.nil?
+    left_depth = depth(node.left)
+    right_depth = depth(node.right)
+    return (left_depth - right_depth).abs <= 1
+  end
+
+  def rebalance!(node = @root)
+    @root = Tree.build_tree(level_order)
+  end
+
   private
   
   def self.build_tree(array, start = 0, ending = array.length)
+    array = array.uniq.sort
     return if (start > ending)
     mid = start + ((ending - start) / 2)
     return if array[mid].nil?
@@ -109,15 +173,3 @@ class Tree
     current
   end
 end
-
-
-a = [1,2,3,4,5,6,7,8,9]
-tree = Tree.new(a)
-p tree
-tree.insert(20)
-p tree
-tree.insert(6)
-p tree
-p tree.find(9)
-tree.delete(5)
-p tree
